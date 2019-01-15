4374923eb2219da316bfb53d7fbf7367cd33406b
Jason Durfee <durfjaso@isu.edu>
2019-01-15 10:55:39 -0700

    update
    added a level 1 and background for it

--- Glitch-Garden/Assets/Scripts/LoadingText.cs
+++ Glitch-Garden/Assets/Scripts/LoadingText.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadingText : MonoBehaviour {
     int dotCount = 0;
@@ -12,10 +13,13 @@ public class LoadingText : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         loadingText = GetComponent<TextMeshProUGUI>();
-        StartCoroutine(Loading());
+        if (SceneManager.GetActiveScene().buildIndex == 0) {
+            StartCoroutine(LoadingWithProgrammedDelay());
+        }
     }
 
-    private IEnumerator Loading() {
+    private IEnumerator LoadingWithProgrammedDelay() {
+        StartCoroutine(Timer(3));
         while (!loadingDone) {
             dotCount++;
             yield return new WaitForSeconds(0.5f);
@@ -26,11 +30,13 @@ public class LoadingText : MonoBehaviour {
             if (dotCount == 3) {
                 dotCount = -1;
             }
-            if (Time.time > 4) {
-                loadingDone = true;
-            }
         }
         FindObjectOfType<LevelLoader>().LoadNextScene();
+    }
+
+    private IEnumerator Timer(float timeToWait) {
+        yield return new WaitForSeconds(timeToWait);
+        loadingDone = true;
     }
 
     // Update is called once per frame
